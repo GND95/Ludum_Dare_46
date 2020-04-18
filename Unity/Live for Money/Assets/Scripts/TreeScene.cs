db305e94f8df988b30d52e3bc76bf48133a70485
Garrett <Garrettdeblois13@yahoo.com>
2020-04-18 17:18:11 -0500

    Fixed tilemap grid scale to be more appropriate. Created logic to dynamically add tiles to the tree scene to grow the tree on button press.

--- Unity/Live for Money/Assets/Scripts/TreeScene.cs
+++ Unity/Live for Money/Assets/Scripts/TreeScene.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
 public class TreeScene : MonoBehaviour
 {
     public Text waterQuantityText;
-   
+
+    public Tile treeBase;
+    public Tilemap tileMap;
+
+    Vector3Int CenterOfMap = new Vector3Int(9, -4, 0);
+    Vector3Int BottomCenterOfMap = new Vector3Int(9, -14, 0);
+
+    public void GrowTree()
+    {
+        tileMap.SetTile(BottomCenterOfMap, treeBase);
+        BottomCenterOfMap.y++;
+    }
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+   
     void Update()
     {
         waterQuantityText.text = PlayerScript.waterQuantity.ToString();
