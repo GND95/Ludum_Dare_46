312745821eec2a7784fe86ad760c4961c1a3852a
Garrett <Garrettdeblois13@yahoo.com>
2020-04-19 00:08:56 -0500

    Wrote warning message and corresponding UI elements for when users switch scenes.

--- Unity/Live for Money/Assets/Scripts/ChangeScene.cs
+++ Unity/Live for Money/Assets/Scripts/ChangeScene.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public GameObject switchScenePanel;
 
     public void changeToSceneNumber(string sceneToChangeTo)
     {        
         SceneManager.LoadScene(sceneToChangeTo);        
+    }
+
+    public void changeSceneWarning()
+    {
+        switchScenePanel.SetActive(false);
+    }
+
+    public void openChangeScenePanel()
+    {
+        switchScenePanel.SetActive(true);
     }
 }
