6eeca33b881360b54f8149da9d6f97e47c511ca6
Garrett <Garrettdeblois13@yahoo.com>
2020-04-19 18:09:39 -0500

    Moved the achievement booleans to the PlayerScript to not trigger the same achievement more than once.

--- Unity/Live for Money/Assets/Scripts/AchievementSystemScript.cs
+++ Unity/Live for Money/Assets/Scripts/AchievementSystemScript.cs
@@ -7,62 +7,61 @@ using UnityEngine.UI;
 public class AchievementSystemScript : MonoBehaviour
 {
     public Text achievementText;
-    public GameObject achievementPanel;
-    private static bool unlocked_1, unlocked_5, unlocked_10, unlocked_25, unlocked_50, unlocked_100, unlocked_250, unlocked_500, unlocked_1000;
+    public GameObject achievementPanel;    
 
     public void CheckAchievementStatus()
     {
-        if (PlayerScript.moneyQuantity >= 1000 && unlocked_1000 == false)
+        if (PlayerScript.moneyQuantity >= 1000 && PlayerScript.unlocked_1000 == false)
         {
-            unlocked_1000 = true;
+            PlayerScript.unlocked_1000 = true;
             achievementText.text = "No Life";
             StartCoroutine(ShowAchievement());            
         }
-        else if (PlayerScript.moneyQuantity >= 500 && unlocked_500 == false)
+        else if (PlayerScript.moneyQuantity >= 500 && PlayerScript.unlocked_500 == false)
         {
-            unlocked_500 = true;
+            PlayerScript.unlocked_500 = true;
             achievementText.text = "Seek Help";
             StartCoroutine(ShowAchievement());
         }
-        else if (PlayerScript.moneyQuantity >= 250 && unlocked_250 == false)
+        else if (PlayerScript.moneyQuantity >= 250 && PlayerScript.unlocked_250 == false)
         {
-            unlocked_250 = true;
+            PlayerScript.unlocked_250 = true;
             achievementText.text = "Seriously. Stop.";
             StartCoroutine(ShowAchievement());
         }
-        else if (PlayerScript.moneyQuantity >= 100 && unlocked_100 == false)
+        else if (PlayerScript.moneyQuantity >= 100 && PlayerScript.unlocked_100 == false)
         {
-            unlocked_100 = true;
+            PlayerScript.unlocked_100 = true;
             achievementText.text = "Why are you still playing this?";
             StartCoroutine(ShowAchievement());
         }
-        else if (PlayerScript.moneyQuantity >= 50 && unlocked_50 == false)
+        else if (PlayerScript.moneyQuantity >= 50 && PlayerScript.unlocked_50 == false)
         {
-            unlocked_50 = true;
+            PlayerScript.unlocked_50 = true;
             achievementText.text = "The Businessman";
             StartCoroutine(ShowAchievement());
         }
-        else if (PlayerScript.moneyQuantity >= 25 && unlocked_25 == false)
+        else if (PlayerScript.moneyQuantity >= 25 && PlayerScript.unlocked_25 == false)
         {
-            unlocked_25 = true;
+            PlayerScript.unlocked_25 = true;
             achievementText.text = "Big Bucks";
             StartCoroutine(ShowAchievement());
         }
-        else if (PlayerScript.moneyQuantity >= 10 && unlocked_10 == false)
+        else if (PlayerScript.moneyQuantity >= 10 && PlayerScript.unlocked_10 == false)
         {
-            unlocked_10 = true;
+            PlayerScript.unlocked_10 = true;
             achievementText.text = "Double Digits";
             StartCoroutine(ShowAchievement());
         }
-        else if (PlayerScript.moneyQuantity >= 5 && unlocked_5 == false)
+        else if (PlayerScript.moneyQuantity >= 5 && PlayerScript.unlocked_5 == false)
         {
-            unlocked_5 = true;
+            PlayerScript.unlocked_5 = true;
             achievementText.text = "Minimum Wage";
             StartCoroutine(ShowAchievement());
         }
-        else if (PlayerScript.moneyQuantity >= 1 && unlocked_1 == false)
+        else if (PlayerScript.moneyQuantity >= 1 && PlayerScript.unlocked_1 == false)
         {
-            unlocked_1 = true;
+            PlayerScript.unlocked_1 = true;
             achievementText.text = "First Dollar";
             StartCoroutine(ShowAchievement());
         }       
--- Unity/Live for Money/Assets/Scripts/PlayerScript.cs
+++ Unity/Live for Money/Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,7 @@ public class PlayerScript : MonoBehaviour
 {
     public static double waterQuantity;
     public static double moneyQuantity;
+    public static bool unlocked_1, unlocked_5, unlocked_10, unlocked_25, unlocked_50, unlocked_100, unlocked_250, unlocked_500, unlocked_1000;
     // Start is called before the first frame update
     void Start()
     {
